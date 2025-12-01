b5f956430a12ed45057f9c2542f87deabb86b184
delowarshizar <husain22205101453@diu.edu.bd>
2025-12-02 01:00:59 +0600

    update

--- script.js
+++ script.js
@@ -6,18 +6,21 @@ const loadWord = (id) => {
 };
 
 const displayWord = (words) => {
-    const wordContainer = document.getElementById('word-container')
-    wordContainer.innerHTML = ""
-    words.forEach(word => {
-        const newWord = document.createElement('div')
-        newWord.innerHTML = `<div class="bg-white p-5 text-center rounded-xl shadow-sm">
-          <h1>${word.word}</h1>
-          <p>Meaning /Pronounciation</p>
-          <p>${word.meaning}</p>
-        </div>`
-        wordContainer.appendChild(newWord)
-    });
-    
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+  words.forEach((word) => {
+    const newWord = document.createElement("div");
+    newWord.innerHTML = `<div class="bg-white p-5 text-center rounded-xl shadow-sm">
+          <h1 class="font-bold text-xl">${word.word}</h1>
+          <p class="font-semibold mt-2">Meaning /Pronounciation</p>
+          <p class="font-semibold text-[18181B] text-xl font-bangla mt-2">${word.meaning}</p>
+           <div class="justify-between flex">
+        <button class="bg-gray-100 w-12  h-12 rounded-xl  hover:bg-gray-400" ><i class="fa-solid fa-circle-info"></i></button>
+        <button class="bg-gray-100  w-12  h-12  rounded-xl hover:bg-gray-400"><i class="fa-solid fa-volume-low"></i></button>
+      </div>
+        </div>`;
+    wordContainer.appendChild(newWord);
+  });
 };
 
 const loadLesson = () => {
